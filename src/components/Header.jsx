7ea08316107190ce2811f2ab7980f186830f57ed
Jabulani Sifundza <jjsifundza@gmail.com>
2023-03-20 09:17:00 +0200

    New Portfolio edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,19 +11,19 @@ const Header = () =>{
 				<img src={myLogo} alt="" className="myLogo" />
 			</div>
 			<div className="navigate">
-				<button className="navBtn" onClick={()=> window.scrollBy(0, 780, {behavior: 'smooth'})}>About</button>
-				<button className="navBtn" onClick={()=> window.scrollBy(0, 2480, {behavior: 'smooth'})}>Projects</button>
-				<button className="navBtn" onClick={()=> window.scrollBy(0, 3380, {behavior: 'smooth'})}>Resume</button>
-				<button className="navBtn" onClick={()=> window.scrollBy(0, 3380, {behavior: 'smooth'})}>Contact</button>
+				<button className="navBtn" onClick={()=> window.scrollBy(0, 790, {behavior: 'smooth'})}>About</button>
+				<button className="navBtn" onClick={()=> window.scrollBy(0, 3420, {behavior: 'smooth'})}>Projects</button>
+				<button className="navBtn" onClick={()=> window.scrollBy(0, 4280, {behavior: 'smooth'})}>Resume</button>
+				<button className="navBtn" onClick={()=> window.scrollBy(0, 4280, {behavior: 'smooth'})}>Contact</button>
 			</div>
 
 			<div className="mobiNav">
 				<img src={toggle ? close : menu} alt="Menu" onClick={()=>setToggle(!toggle)} />
 				<div className={`${!toggle ? "hidden":"flex"}`}>
-					<button className="navBtn" onClick={()=> window.scrollBy(0, 780, {behavior: 'smooth'})}>About</button>
-					<button className="navBtn" onClick={()=> window.scrollBy(0, 2480, {behavior: 'smooth'})}>Projects</button>
-					<button className="navBtn" onClick={()=> window.scrollBy(0, 3380, {behavior: 'smooth'})}>Resume</button>
-					<button className="navBtn" onClick={()=> window.scrollBy(0, 3380, {behavior: 'smooth'})}>Contact</button>
+					<button className="navBtn" onClick={()=> window.scrollBy(0, 790, {behavior: 'smooth'})}>About</button>
+					<button className="navBtn" onClick={()=> window.scrollBy(0, 3420, {behavior: 'smooth'})}>Projects</button>
+					<button className="navBtn" onClick={()=> window.scrollBy(0, 4280, {behavior: 'smooth'})}>Resume</button>
+					<button className="navBtn" onClick={()=> window.scrollBy(0, 4280, {behavior: 'smooth'})}>Contact</button>
 				</div>
 			</div>
 		</header>
